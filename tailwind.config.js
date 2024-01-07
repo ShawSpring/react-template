366d5895903e700f9a58a4d51f628a306733ca27
ShawSpring <chunlaixiao@163.com>
2024-01-08 04:15:40 +0800

    add nth

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
+import plugin from 'tailwindcss/plugin';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchVariant }) {
+      matchVariant(
+        'nth',
+        (value) => {
+          return `&:nth-child(${value})`;
+        },
+        {
+          values: {
+            1: '1',
+            2: '2',
+            3: '3',
+          },
+        }
+      );
+    }),
+  ],
 };
